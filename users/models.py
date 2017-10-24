f929f8d9de74c509e70938679384fceb5ff8bf1d
Jaxon <caozhiqiango@foxmail.com>
2017-10-24 13:54:31 +0800

    完善用户相关联的部分模型设计

--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-
+# 用户信息模型
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
     birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
@@ -21,3 +22,28 @@ class UserProfile(AbstractUser):
 
     def __unicode__(self):
         return self.username
+
+
+# 邮箱验证码模型
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
+    send_type = models.CharField(max_length=10, choices=(("register", u"注册"), ("forget", u"找回密码")))
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = u"邮箱验证码"
+        verbose_name_plural = verbose_name
+
+
+# 首页轮播模型
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u"标题")
+    image = models.ImageField(upload_to="banner/%Y/%m", verbose_name=u"轮播图")
+    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    index = models.IntegerField(default=100, verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"轮播图"
+        verbose_name_plural = verbose_name
